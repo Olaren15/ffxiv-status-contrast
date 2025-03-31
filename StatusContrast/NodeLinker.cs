83d0e911d3092c43c5bdb9dbc2c914d7cd43bc48
Catherine Gilbert <catherine.gilbert15@gmail.com>
2025-03-30 23:23:08 -0400

    Remove KamiToolKit dependency

---
+++ StatusContrast/NodeLinker.cs
@@ -0,0 +1,78 @@
+﻿using FFXIVClientStructs.FFXIV.Component.GUI;
+
+namespace StatusContrast;
+
+/**
+ * The following code is extracted from MidoriKami's KamiToolKit. Copyright 2024 MidoriKami.
+ * For licensing see /LICENSE/LICENSE-KamiToolKit.txt
+ */
+public static unsafe class NodeLinker
+{
+    public static void AttachToNode(AtkResNode* node, AtkResNode* attachTargetNode)
+    {
+        // If the child list is empty
+        if (attachTargetNode->ChildNode is null && attachTargetNode->ChildCount is 0)
+        {
+            if ((int)attachTargetNode->Type < 1000)
+            {
+                attachTargetNode->ChildNode = node;
+                node->ParentNode = attachTargetNode;
+                attachTargetNode->ChildCount++;
+            }
+            else
+            {
+                node->ParentNode = attachTargetNode;
+            }
+        }
+        // Else Add to the List as the First Child
+        else
+        {
+            if ((int)attachTargetNode->Type < 1000)
+            {
+                attachTargetNode->ChildNode->NextSiblingNode = node;
+                node->PrevSiblingNode = attachTargetNode->ChildNode;
+                attachTargetNode->ChildNode = node;
+                node->ParentNode = attachTargetNode;
+                attachTargetNode->ChildCount++;
+            }
+            else
+            {
+                node->PrevSiblingNode = attachTargetNode->ChildNode;
+                node->ParentNode = attachTargetNode;
+            }
+        }
+    }
+
+    public static void DetachNode(AtkResNode* node)
+    {
+        if (node is null)
+        {
+            return;
+        }
+
+        if (node->ParentNode is null)
+        {
+            return;
+        }
+
+        if (node->ParentNode->ChildNode == node)
+        {
+            node->ParentNode->ChildNode = node->PrevSiblingNode;
+        }
+
+        if (node->PrevSiblingNode != null)
+        {
+            node->PrevSiblingNode->NextSiblingNode = node->NextSiblingNode;
+        }
+
+        if (node->NextSiblingNode != null)
+        {
+            node->NextSiblingNode->PrevSiblingNode = node->PrevSiblingNode;
+        }
+
+        if ((int)node->ParentNode->Type < 1000)
+        {
+            node->ParentNode->ChildCount--;
+        }
+    }
+}
--- StatusContrast/Plugin.cs
+++ StatusContrast/Plugin.cs
@@ -1,7 +1,10 @@
-﻿using Dalamud.Interface.Windowing;
+﻿using Dalamud.Game.Addon.Lifecycle;
 using Dalamud.IoC;
 using Dalamud.Plugin;
 using Dalamud.Plugin.Services;
+using FFXIVClientStructs.FFXIV.Client.Graphics;
+using FFXIVClientStructs.FFXIV.Client.System.Memory;
+using FFXIVClientStructs.FFXIV.Client.UI;
 using FFXIVClientStructs.FFXIV.Component.GUI;
 
 namespace StatusContrast;
@@ -9,22 +12,55 @@ namespace StatusContrast;
 // ReSharper disable once UnusedType.Global
 public sealed unsafe class Plugin : IDalamudPlugin
 {
-    private readonly BackgroundNodes _backgroundNodes;
-    private readonly WindowSystem _windowSystem = new("StatusContrast");
+    private AtkUnitBase* _buffs;
+    private AtkImageNode* _buffsBackground = null;
+
+    private AtkUnitBase* _debuffs;
+    private AtkImageNode* _debuffsBackground = null;
+
+    private AtkImageNode* _jobStatusBackground = null;
+    private AtkUnitBase* _jobStatuses;
+
+    private AddonNamePlate* _namePlate;
+
+    private AtkUnitBase* _otherStatuses;
+    private AtkImageNode* _otherStatusesBackground = null;
 
     public Plugin()
     {
-        PluginInterface.UiBuilder.Draw += DrawUi;
-        AtkUnitBase*[] statusesAtk =
-        [
-            (AtkUnitBase*)GameGui.GetAddonByName("_StatusCustom0"),
-            (AtkUnitBase*)GameGui.GetAddonByName("_StatusCustom1"),
-            (AtkUnitBase*)GameGui.GetAddonByName("_StatusCustom2"),
-            (AtkUnitBase*)GameGui.GetAddonByName("_StatusCustom3")
-        ];
+        // Try to get addons if plugin is loaded when the ui is loaded
+        _namePlate = (AddonNamePlate*)GameGui.GetAddonByName("NamePlate");
+        _buffs = (AtkUnitBase*)GameGui.GetAddonByName("_StatusCustom0");
+        _debuffs = (AtkUnitBase*)GameGui.GetAddonByName("_StatusCustom1");
+        _otherStatuses = (AtkUnitBase*)GameGui.GetAddonByName("_StatusCustom2");
+        _jobStatuses = (AtkUnitBase*)GameGui.GetAddonByName("_StatusCustom3");
 
-        _backgroundNodes = new BackgroundNodes(statusesAtk, AddonLifecycle, Framework, GameGui, PluginInterface);
-        _backgroundNodes.Enable();
+        // Otherwise wait until ui is available
+        AddonLifecycle.RegisterListener(AddonEvent.PostSetup, "NamePlate",
+            (_, args) => _namePlate = (AddonNamePlate*)args.Addon);
+        AddonLifecycle.RegisterListener(AddonEvent.PreFinalize, "NamePlate", (_, _) =>
+        {
+            DestroyBackgrounds();
+            _namePlate = null;
+        });
+
+        AddonLifecycle.RegisterListener(AddonEvent.PostSetup, "_StatusCustom0",
+            (_, args) => _buffs = (AtkUnitBase*)args.Addon);
+        AddonLifecycle.RegisterListener(AddonEvent.PreFinalize, "_StatusCustom0", (_, _) => _buffs = null);
+
+        AddonLifecycle.RegisterListener(AddonEvent.PostSetup, "_StatusCustom1",
+            (_, args) => _debuffs = (AtkUnitBase*)args.Addon);
+        AddonLifecycle.RegisterListener(AddonEvent.PreFinalize, "_StatusCustom1", (_, _) => _debuffs = null);
+
+        AddonLifecycle.RegisterListener(AddonEvent.PostSetup, "_StatusCustom2",
+            (_, args) => _otherStatuses = (AtkUnitBase*)args.Addon);
+        AddonLifecycle.RegisterListener(AddonEvent.PreFinalize, "_StatusCustom2", (_, _) => _otherStatuses = null);
+
+        AddonLifecycle.RegisterListener(AddonEvent.PostSetup, "_StatusCustom3",
+            (_, args) => _jobStatuses = (AtkUnitBase*)args.Addon);
+        AddonLifecycle.RegisterListener(AddonEvent.PreFinalize, "_StatusCustom3", (_, _) => _jobStatuses = null);
+
+        Framework.Update += Update;
     }
 
     // ReSharper disable once AutoPropertyCanBeMadeGetOnly.Local
@@ -42,15 +78,173 @@ public sealed unsafe class Plugin : IDalamudPlugin
     // ReSharper disable once AutoPropertyCanBeMadeGetOnly.Local
     [PluginService] private static IPluginLog Log { get; set; } = null!;
 
-
     public void Dispose()
     {
-        _windowSystem.RemoveAllWindows();
-        _backgroundNodes.Dispose();
+        DestroyBackgrounds();
+        Framework.Update -= Update;
     }
 
-    private void DrawUi()
+    private void Update(IFramework framework)
     {
-        _windowSystem.Draw();
+        if (_namePlate is null)
+        {
+            Log.Info("nameplate not ready");
+            return;
+        }
+
+        if (_buffs is not null && _buffsBackground is null)
+        {
+            Log.Info("creating buffs background");
+            _buffsBackground = CreateBackground(_buffs);
+            NodeLinker.AttachToNode((AtkResNode*)_buffsBackground, _namePlate->RootNode);
+            _namePlate->UldManager.UpdateDrawNodeList();
+        }
+
+        if (_debuffs is not null && _debuffsBackground is null)
+        {
+            Log.Info("creating debuff background");
+            _debuffsBackground = CreateBackground(_debuffs);
+            NodeLinker.AttachToNode((AtkResNode*)_debuffsBackground, _namePlate->RootNode);
+            _namePlate->UldManager.UpdateDrawNodeList();
+        }
+
+        if (_otherStatuses is not null && _otherStatusesBackground is null)
+        {
+            Log.Info("creating other statuses background");
+            _otherStatusesBackground = CreateBackground(_otherStatuses);
+            NodeLinker.AttachToNode((AtkResNode*)_otherStatusesBackground, _namePlate->RootNode);
+            _namePlate->UldManager.UpdateDrawNodeList();
+        }
+
+        if (_jobStatuses is not null && _jobStatusBackground is null)
+        {
+            Log.Info("creating job statuses background");
+            _jobStatusBackground = CreateBackground(_jobStatuses);
+            NodeLinker.AttachToNode((AtkResNode*)_jobStatusBackground, _namePlate->RootNode);
+            _namePlate->UldManager.UpdateDrawNodeList();
+        }
+    }
+
+    private void DestroyBackgrounds()
+    {
+        if (_buffsBackground is not null)
+        {
+            Log.Info("destroying buffs background");
+            Framework.RunOnFrameworkThread(() =>
+            {
+                NodeLinker.DetachNode((AtkResNode*)_buffsBackground);
+                DestroyBackground(_buffsBackground);
+                _buffsBackground = null;
+
+                _namePlate->UldManager.UpdateDrawNodeList();
+            });
+        }
+
+        if (_debuffsBackground is not null)
+        {
+            Log.Info("destroying debuffs background");
+            Framework.RunOnFrameworkThread(() =>
+            {
+                NodeLinker.DetachNode((AtkResNode*)_debuffsBackground);
+                DestroyBackground(_debuffsBackground);
+                _debuffsBackground = null;
+
+                _namePlate->UldManager.UpdateDrawNodeList();
+            });
+        }
+
+        if (_otherStatusesBackground is not null)
+        {
+            Log.Info("destroying other statuses background");
+            Framework.RunOnFrameworkThread(() =>
+            {
+                NodeLinker.DetachNode((AtkResNode*)_otherStatusesBackground);
+                DestroyBackground(_otherStatusesBackground);
+                _otherStatusesBackground = null;
+
+                _namePlate->UldManager.UpdateDrawNodeList();
+            });
+        }
+
+        if (_jobStatusBackground is not null)
+        {
+            Log.Info("destroying job statuses background");
+            Framework.RunOnFrameworkThread(() =>
+            {
+                NodeLinker.DetachNode((AtkResNode*)_jobStatusBackground);
+                DestroyBackground(_jobStatusBackground);
+                _jobStatusBackground = null;
+
+                _namePlate->UldManager.UpdateDrawNodeList();
+            });
+        }
+    }
+
+    private static AtkImageNode* CreateBackground(AtkUnitBase* statusesBar)
+    {
+        AtkUldAsset* asset = (AtkUldAsset*)IMemorySpace.GetUISpace()->Malloc<AtkUldAsset>();
+        if (asset is null)
+        {
+            return null;
+        }
+
+        IMemorySpace.Memset(asset, 0, (ulong)sizeof(AtkUldAsset));
+        asset->AtkTexture.Ctor();
+
+        AtkUldPart* part = (AtkUldPart*)IMemorySpace.GetUISpace()->Malloc<AtkUldPart>();
+        if (part is null)
+        {
+            IMemorySpace.Free(asset);
+            return null;
+        }
+
+        IMemorySpace.Memset(part, 0, (ulong)sizeof(AtkUldPart));
+        part->UldAsset = asset;
+
+        AtkUldPartsList* parts = (AtkUldPartsList*)IMemorySpace.GetUISpace()->Malloc<AtkUldPartsList>();
+        if (parts is null)
+        {
+            IMemorySpace.Free(asset);
+            IMemorySpace.Free(part);
+            return null;
+        }
+
+        parts->Parts = part;
+        parts->PartCount = 1;
+        parts->Id = 0;
+
+        AtkImageNode* node = IMemorySpace.GetUISpace()->Create<AtkImageNode>();
+        if (parts is null)
+        {
+            IMemorySpace.Free(asset);
+            IMemorySpace.Free(parts);
+            IMemorySpace.Free(part);
+            return null;
+        }
+
+        node->Type = NodeType.Image;
+        node->Flags = (byte)ImageNodeFlags.AutoFit;
+        node->WrapMode = 0x1;
+        node->NodeFlags = NodeFlags.Visible;
+        node->Color = new ByteColor { R = 0, G = 0, B = 0, A = 128 };
+        node->SetXFloat(statusesBar->RootNode->X);
+        node->SetYFloat(statusesBar->RootNode->Y);
+        node->SetWidth(statusesBar->RootNode->Width);
+        node->SetHeight(statusesBar->RootNode->Height);
+        node->SetScale(statusesBar->RootNode->ScaleX, statusesBar->RootNode->ScaleY);
+
+        node->PartsList = parts;
+
+        return node;
+    }
+
+    private static void DestroyBackground(AtkImageNode* node)
+    {
+        IMemorySpace.Free(node->PartsList->Parts->UldAsset);
+        IMemorySpace.Free(node->PartsList->Parts);
+        IMemorySpace.Free(node->PartsList);
+
+        node->Destroy(false);
+        IMemorySpace.Free(node);
     }
 }
