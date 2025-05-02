fc5b7138704edfdc3fdf0bc119dc06041a7e7486
Catherine Gilbert <mail@cathgilbert.com>
2025-05-01 22:41:28 -0400

    Improve settings window

--- StatusContrast/SettingsWindow.cs
+++ StatusContrast/SettingsWindow.cs
@@ -19,7 +19,7 @@ public class SettingsWindow : Window
 
         SizeConstraints = new WindowSizeConstraints
         {
-            MinimumSize = new Vector2(200, 100), MaximumSize = new Vector2(float.MaxValue, float.MaxValue)
+            MinimumSize = new Vector2(400, 200), MaximumSize = new Vector2(float.MaxValue, float.MaxValue)
         };
 
         Configuration config = _configurationRepository.GetConfiguration();
@@ -37,20 +37,17 @@ public class SettingsWindow : Window
     public override void Draw()
     {
         ImGui.Checkbox("Preview", ref _preview);
+        ImGui.SameLine();
+        Help("Preview what the backgrounds would look like if they were all active");
+
 
         ImGui.Checkbox("Fix Gaps", ref _fixGaps);
         ImGui.SameLine();
-        ImGui.TextDisabled("(?)");
-        if (ImGui.IsItemHovered())
-        {
-            using (ImRaii.Tooltip())
-            {
-                ImGui.TextUnformatted("For some reason, the game has inconsistent spacing between status icons.\n" +
-                                      "You probably want this setting on, but the toggle is here if you want to disable it");
-            }
-        }
+        Help("For some reason, the game has inconsistent spacing between status icons.\n" +
+             "You probably want this setting on, but the toggle is here if you want to disable it");
 
-        ImGui.ColorEdit4("Background", ref _color,
+        ImGui.TextUnformatted("Background Color:");
+        ImGui.ColorEdit4("Color", ref _color,
             ImGuiColorEditFlags.InputRGB | ImGuiColorEditFlags.PickerHueBar | ImGuiColorEditFlags.AlphaBar |
             ImGuiColorEditFlags.Float | ImGuiColorEditFlags.DisplayRGB);
 
@@ -58,5 +55,19 @@ public class SettingsWindow : Window
         {
             Version = 1, Preview = _preview, FixGaps = _fixGaps, Color = _color
         });
+    }
+
+    private static void Help(string helpText)
+    {
+        ImGui.TextDisabled("(?)");
+        if (!ImGui.IsItemHovered())
+        {
+            return;
+        }
+
+        using (ImRaii.Tooltip())
+        {
+            ImGui.TextUnformatted(helpText);
+        }
     }
 }
